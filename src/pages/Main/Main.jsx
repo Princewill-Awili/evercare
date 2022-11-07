1d2cf76a006d122c0a7b9e51e7bcca06ea1c190c
Princewill-Awili <princewill.awili@gmail.com>
2022-11-07 15:39:12 +0100

    Login Access Page UI completed

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,6 +1,8 @@
 import  './main.css'
 import Logo from "../../assets/evercareLogo2.png"
 import { useState } from 'react'
+import {BsEnvelopeFill as EnvelopeIcon} from 'react-icons/bs'
+import {AiFillLock as LockIcon} from 'react-icons/ai' 
 
 const Main = () => {
     const [accessType, setAccessType] = useState('patient');
@@ -22,16 +24,66 @@ const Main = () => {
                         }
                     </div>
                     <div className="aForm">
-                        <select name="hospital" id="hospital" className='hospital aFormInput'>
-                            <option value="Evercare Hospital Lekki">Evercare Hospital Lekki</option>
-                        </select>
-                        <div className="inputWrapper">
-                            <input type="text" className="uuid aFormInput"  placeholder='Enter UUID'/>
+                        {
+                            accessType === 'patient' ? (
+                                <>
+                                     <select name="hospital" id="hospital" className='hospital aFormSelect'>
+                                            <option value="Evercare Hospital Lekki">Evercare Hospital Lekki</option>
+                                            </select>
+                                            <div className="inputWrapper">
+                                                <input type="text" className="uuid aFormInput"  placeholder='Enter UHID'/>
+                                                <EnvelopeIcon className='inputIcon'/>
+                                            </div>
+                                            <div className="inputWrapper">
+                                                <input type="text" className="password aFormInput" placeholder='Enter password'/>
+                                                <LockIcon className='inputIcon'/>
+                                            </div>
 
-                        </div>
-                        <div className="inputWrapper">
-                            <input type="text" className="password aFormInput" placeholder='Enter password'/>
-                        </div>
+                                            <div className="aFormBtns">
+                                                <div className="loginBtn">Login</div>
+                                                <div className="resetBtn">Reset</div>
+                                            </div>
+
+                                            <div className="forgotBtn miscBtns">Forgot Password?</div>
+
+                                            <div className="howToUse miscBtns"><a href='https://www.youtube.com/watch?v=egQj_MSSaX0' target="blank">How to use the Portal</a></div>
+                                </>
+
+                            )
+                            :
+                            (
+                                <>
+                                            <select name="hospital" id="hospital" className='hospital aFormSelect'>
+                                                <option value="Department">Department</option>
+                                                <option value="Cardiology">Cardiology</option>
+                                                <option value="Neurosurgery">Neurosurgery</option>
+                                                <option value="Family Medicine">Family Medicine</option>
+                                                <option value="Gynaecology">Gynaecology</option>
+                                                <option value="Radiology">Radiology</option>
+                                                <option value="Optometry">Optometry</option>
+                                                <option value="Accidents & Emergency">Accidents & Emergency</option>
+                                            </select>
+                                            <div className="inputWrapper">
+                                                <input type="text" className="uuid aFormInput"  placeholder='Enter Staff ID'/>
+                                                <EnvelopeIcon className='inputIcon'/>
+                                            </div>
+                                            <div className="inputWrapper">
+                                                <input type="text" className="password aFormInput" placeholder='Enter password'/>
+                                                <LockIcon className='inputIcon'/>
+                                            </div>
+
+                                            <div className="aFormBtns">
+                                                <div className="loginBtn">Login</div>
+                                                <div className="resetBtn">Reset</div>
+                                            </div>
+
+                                            <div className="forgotBtn miscBtns">Forgot Password?</div>
+
+                                            <div className="howToUse miscBtns"><a href='https://www.youtube.com/watch?v=egQj_MSSaX0' target="blank">How to use the Portal</a></div>
+                                </>
+                            )
+                        }
+                       
                         
                         
                     </div>
