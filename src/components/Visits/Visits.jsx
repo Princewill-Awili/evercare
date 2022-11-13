ba52c940d6cdb555308f623693f083b73d32eb05
Princewill Awili <princewill.awili@gmail.com>
2022-11-13 12:30:24 +0100

    Implementing Visits Note

--- src/components/Visits/Visits.jsx
+++ src/components/Visits/Visits.jsx
@@ -1,19 +1,108 @@
 import './visits.css'
 import {FaHistory as HistoryIcon} from 'react-icons/fa'
+import {GoCalendar as SummaryIcon} from 'react-icons/go'
 import TitleBar from '../TitleBar/TitleBar'
+import { useState } from 'react'
+import Logo from '../../assets/evercareLogo2.png'
 
 const Visits = () => {
+
+  const [showNotes,setShowNotes] = useState(false);
+  const [note,setNote] = useState({})
+
+  const user = JSON.parse(localStorage.getItem('user'));
+  const {visits} = user;
+
+
+  const VLog =({serial, date,time, doctor, specialization, vType, vSummary})=>{
+
+    const displaySummary =(e)=>{
+      const selectedNote = visits.find(visit => visit.id === e.currentTarget.id);
+      setNote(selectedNote);
+      setShowNotes(!showNotes);
+    }
+
+
+    return(
+      <div className="vLog member">
+          <span className="sNum header">{serial}</span>
+          <span className="date header">{`${date} ${time}`}</span>
+          <span className="doc header">{`${doctor}(${specialization})`}</span>
+          <span className="vType header">{vType}</span>
+          <span className="vSummary header">
+            <SummaryIcon id={serial} className="notesIcon" onClick={displaySummary}/>
+          </span>
+      </div>
+
+    )
+  }
+
   return (
     <div className='visits'>
       <TitleBar svg={<HistoryIcon/>} txt="Recent Doctor Visits"/>
       <div className="visitLogsContainer">
-        <div className="VlogHeader">
-          <span className="sNum header">SI.No</span>
-          <span className="date header">Date & Time</span>
-          <span className="doc header">Doctor Name & Specialization</span>
-          <span className="vType header">Visit Type</span>
-          <span className="vSummary header">OPD Summary</span>
-        </div>
+        {
+          showNotes === false && 
+          (
+            <>
+            <div className="VlogHeader member">
+              <span className="sNum header">S/No.</span>
+              <span className="date header">Date & Time</span>
+              <span className="doc header">Doctor Name & Specialization</span>
+              <span className="vType header">Visit Type</span>
+              <span className="vSummary header">Summary</span>
+            </div>
+  
+          {
+            visits.map((visit,index)=>(
+              <VLog 
+                key={index} 
+                serial={visit.id} 
+                date={visit.date} 
+                time={visit.time}
+                doctor={visit.doctor}
+                specialization={visit.specialization}
+                vType={visit.visitType}
+                vSummary={""}
+              />
+            ))
+          }
+          </>
+          )
+        }
+
+        {
+          showNotes && (
+            <div className="note">
+              <img src={Logo} className='noteLogo'/>
+              <div className="letterHead">
+                <h3>Evercare Hospital Lekki, Ltd.</h3>
+                <p className="address">1, Admiralty Way, Bisola Durosimi Etti Street, Lekki Phase 1,Lekki, Lagos.</p>
+                <p className="tel">Phone: +234 9091223478</p>
+              </div>
+              <h4 className="noteTitle">CONSULTATION SUMMARY</h4>
+              <div className="noteGenInfo">
+                <div className="noteGenInfoLeft">
+                  <p>UHID:</p>
+                  <p>Age/Sex:</p>
+                  <p>Visit No:</p>
+                  <p>Telephone:</p>
+                  <p>Address:</p>
+                </div>
+                <div className="noteGenInfoRight">
+                  <p>Patient Name:</p>
+                  <p>Doctor Name:</p>
+                  <p>Visit Date:</p>
+                  <p>Reffered By:</p>
+                </div>
+                 
+              </div>
+            </div>
+          )
+        }
+        
+
+
       </div>
     </div>
   )
--- src/utils/dummyData.js
+++ src/utils/dummyData.js
@@ -14,6 +14,53 @@ export const sampleUser = {
           blood_group: "O+",
           genotype:"AA"
      },
+     visits:[
+          {
+               id:1,
+               date:"17/05/2022", 
+               time:"12:05PM", 
+               doctor:"Dr. Mirabel Nwosu", 
+               specialization:"Cardiology", 
+               visitType:"OutPatient", 
+               summary:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi beatae temporibus corporis nihil, iusto, veritatis doloribus eum ipsa, expedita sint quos praesentium quo aperiam. Fugit officia perferendis dolorum voluptas aliquid."
+          },
+          {
+               id:2,
+               date:"23/06/2022", 
+               time:"03:00PM", 
+               doctor:"Dr. Vivian Ukomadu", 
+               specialization:"General Practice", 
+               visitType:"Walk In", 
+               summary:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi beatae temporibus corporis nihil, iusto, veritatis doloribus eum ipsa, expedita sint quos praesentium quo aperiam. Fugit officia perferendis dolorum voluptas aliquid."
+          },
+          {
+               id:3,
+               date:"19/08/2022", 
+               time:"11.05AM", 
+               doctor:"Dr. Joy Ekwueme", 
+               specialization:"Optometry", 
+               visitType:"Walk In", 
+               summary:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi beatae temporibus corporis nihil, iusto, veritatis doloribus eum ipsa, expedita sint quos praesentium quo aperiam. Fugit officia perferendis dolorum voluptas aliquid."
+          },
+          {
+               id:4,
+               date:"23/09/2022", 
+               time:"08.05PM", 
+               doctor:"Dr. Enaholo Odia", 
+               specialization:"Family Medicine", 
+               visitType:"OutPatient", 
+               summary:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi beatae temporibus corporis nihil, iusto, veritatis doloribus eum ipsa, expedita sint quos praesentium quo aperiam. Fugit officia perferendis dolorum voluptas aliquid."
+          },
+          {
+               id:5,
+               date:"10/11/2022", 
+               time:"09.00AM", 
+               doctor:"Dr. Ayodeji Odufuwa", 
+               specialization:"General Practice", 
+               visitType:"OutPatient", 
+               summary:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi beatae temporibus corporis nihil, iusto, veritatis doloribus eum ipsa, expedita sint quos praesentium quo aperiam. Fugit officia perferendis dolorum voluptas aliquid."
+          }
+     ],
      diagnostics:[],
      appointments:[],
      payments:[]
